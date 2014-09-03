21b9b43a4d248431f32100ebdea5ccdf96d84c6d
Development Team <dev@digitalspider.com.au>
2014-09-03 20:58:35 +1000

    Cleared out old wiki

--- dswiki/custom/publish.sh
+++ dswiki/custom/publish.sh
@@ -4,4 +4,4 @@ WIKIDIR=$CATALINA_HOME/webapps/JSPWiki
 cp -f ./jspwiki-custom-linux.properties ./WEB-INF/classes/jspwiki-custom.properties
 
 # Copy the WEB-INF directory
-cp -rf ./WEB-INF ${WIKIDIR}/WEB-INF
+cp -rf ./WEB-INF/* ${WIKIDIR}/WEB-INF/
