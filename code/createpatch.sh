45fa76f35b90f6386b4aafb79b14d4edef34c753
Development Team <dev@digitalspider.com.au>
2014-09-17 19:57:31 +1000

    created code dir, patches and SampleAjaxPlugin page

---
+++ code/createpatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#CATALINA_HOME=/opt/tomcat/tomcat
+DSDIR=/opt/digitalspider
+BASEDIR=$DSDIR/jspwiki
+CODEDIR=$DSDIR/code
+PATCHDIR=$CODEDIR/patches
+STAGEDIR=$CODEDIR/jspwiki-stage
+
+cd $STAGEDIR
+svn diff > $PATCHDIR/$1.patch
+cd ..
+
